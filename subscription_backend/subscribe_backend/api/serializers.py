ce8f45beaeff65872aae6c958bd00888e9c08966
Nikhil Tatpati <nikhiltatpati11@gmail.com>
2021-03-12 17:27:59 +0530

    Files updated v2

--- subscription_backend/subscribe_backend/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Subscribe
-
-class SubscribeSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Subscribe
-		fields ='__all__'
